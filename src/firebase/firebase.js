c3167c406def24c7f6e6d240f151f599ae5a6367
ivan.taranenko <vanokalipsi@egmail.com>
2022-08-17 13:47:43 +0300

    create empty project

---
+++ src/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDkAxmtmQOp64V72aoCO9GHCorv-_QKkO0",
+    authDomain: "netflix-project-react-js.firebaseapp.com",
+    projectId: "netflix-project-react-js",
+    storageBucket: "netflix-project-react-js.appspot.com",
+    messagingSenderId: "726117125933",
+    appId: "1:726117125933:web:c3ac6fa4f5f0da82f34da4"
+};
